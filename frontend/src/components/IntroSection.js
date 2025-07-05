fc8103916d882c1e0613115f4cb5bce88919fa27
codeby-riya <codebyriya@gmail.com>
2025-07-05 16:09:15 +0530

    update

--- frontend/src/components/IntroSection.js
+++ frontend/src/components/IntroSection.js
@@ -7,7 +7,7 @@ function IntroSection() {
       <h2 className="text-primary">Welcome to FitInSight Risk Predictor</h2>
       <p>
         This intelligent tool uses <strong>Machine Learning models</strong> to assess your health risks related to
-        <strong> Heart Disease, Obesity,</strong> and <strong>Diabetes</strong>.
+        <strong> Heart Disease</strong> and <strong>Obesity</strong>.
         <br />
         Simply select your desired prediction type below and fill out the quick form to get an instant risk assessment in percentage.
       </p>
